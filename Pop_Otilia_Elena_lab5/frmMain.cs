caf8d9249bad48e595853662c33abefa8ee0033f
elenapop <72506899+elenapop@users.noreply.github.com>
2020-11-18 18:18:26 +0200

    Add files via upload

---
+++ Pop_Otilia_Elena_lab5/frmMain.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Pop_Otilia_Elena_lab5
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
